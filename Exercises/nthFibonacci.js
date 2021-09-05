9419b880d9278826bd833fa4fe00fa30a34cc33b
SandyLOC <39068406+SandyLOC@users.noreply.github.com>
2021-09-04 19:45:54 -0700

    Fibonacci exercise result

--- Exercises/nthFibonacci.js
+++ Exercises/nthFibonacci.js
@@ -17,6 +17,12 @@
 // etc...
 
 var nthFibonacci = function(n) {
-  // Your code here
+  // Your code here:
+    if (n === 0) return 0;
+    if(n <= 1) return 1;
+
+  return nthFibonacci(n-1) + nthFibonacci(n-2);
+}
+console.log(nthFibonacci(10));
 };
 
